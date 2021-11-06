1f4539ba703c3e8868bee0a5a110d4a40a6f0812
Yann Klein <yann.klein@me.com>
2021-11-06 14:23:23 +0900

    livecode strt

--- app.rb
+++ app.rb
@@ -22,3 +22,25 @@ sessions_controller = SessionsController.new(employee_repository)
 
 router = Router.new(meals_controller, customers_controller, sessions_controller)
 router.run
+
+# - [x] As a manager I can list all meals
+# - [x] As a manager I can add a meal
+# - [x] As a manager I can list all customers
+# - [x] As a manager I can add a customer
+# - [ ] As a manager I can list undelivered orders
+# - [ ] As a manager I can add an order
+
+# - [ ] As a rider I can list my undelivered orders
+# - [ ] As a rider I can mark an order as delivered
+
+# +--------------------+
+# |       Order        |
+# +--------------------+
+# | @id                |
+# | @customer          |
+# | @meal              |
+# | @employee          |
+# | @delivered         |
+# +--------------------+
+# | mark_as_delivered! |
+# +--------------------+
